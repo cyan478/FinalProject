7f38e56dbfe925b57b13f3f38c964eaec753bb25
cyan478 <cyan2@stuy.edu>
2016-01-21 03:37:11 -0500

    updated

--- Character.java
+++ Character.java
@@ -1,18 +1,39 @@
 public class Character{
     
-    private int _hp;
+    private int _totalhp; //hp limit
+    private int _hp; //current hp
     private int _atk;
     private int _def;
     //private int _speed;
     private int _exp;
+    private int _currentexp;
     private int _level;
+    private String _name;
 
+    //Constructor
+    //===========
+    //used for creating player
     public Character(){
-	_hp = 10;
+	_totalhp = 10;
+	_hp = _totalhp; //full health
 	_atk = 5;
 	_def = 5;
 	_exp = 0;
+	_currentexp = 0;
 	_level = 1;
+    }
+
+    //Overloaded Constructor
+    //======================
+    //mainly used for opponents 
+    public Character(String name, totalhp, atk, def, lvl){
+	_name = name;
+	_totalhp = totalhp;
+	_hp = _totalhp;
+	_atk = atk;
+	_def = def;
+	_exp = -1;
+	_level = lvl;
     }
 
     //accessors========================================
@@ -38,6 +59,9 @@ public class Character{
     }
     //================================================
     
+    //Crit
+    //=====
+    //aka random roll
     //calculates if player gets a crit (gives more power in method attack(n))
     public int crit(){
     	int critical = _level;
@@ -45,8 +69,11 @@ public class Character{
     	if (r == 16) return 2*critical; //16 is a random number
     	return critical;
     }
-    
-    //algorithm inspired by https://www.math.miami.edu/~jam/azure/compendium/battdam.htm (pokemon)
+
+    //Attacking Algortihm
+    //====================
+    //(pokemon) algorithm inspired by 
+    //https://www.math.miami.edu/~jam/azure/compendium/battdam.htm 
     public int attack(int oppDef){
     	int A = crit(); //attacker's level / whether critical hit is considered
     	int B = _atk; //attacker's atk power
@@ -56,20 +83,10 @@ public class Character{
     	return ((((((((2*A/5+2)*B*B)/C)/50)+2))*Y/10)*Z)/255;
 	}
 
+    //Taking damage
+    //=============
     public void lowerHP(int n){
 	_hp -=n;
-    }
-    
-    public void levelUp(){
-    	int h,a,d;
-    	h = (int)(Math.random() * 10);
-    	a = (int)(Math.random() *5);
-    	d = (int)(Math.random() *5);
-    	
-    	_hp += h;
-    	_atk += a;
-    	_def += d;
-    	_level += 1;
     }
 
 
--- Player.java
+++ Player.java
@@ -1,4 +1,37 @@
 public class Player extends Character{
+ 
+    public Player(String name){
+	super(name);
+	
+    }
+    
+    //Update Character
+    //===============
+    //gainEXP(x) -> if _exp+=exp > _exp limit, levelUp()
+    public void Update(int exp){
+	gainEXP(exp);
+	if (this._currentexp > this._exp) 
+	
+
+
+    //Gaining EXP (Experience Points used to level up)
+    public void gainEXP(int exp){
+	this._currentexp += exp;
+    }
+
+    //Leveling up 
+    public void levelUp(){
+    	int h,a,d;
+    	h = (int)(Math.random() * 10);
+    	a = (int)(Math.random() *5);
+    	d = (int)(Math.random() *5);
+    	
+    	_hp += h;
+    	_atk += a;
+    	_def += d;
+    	_level += 1;
+    }
+
 
 
 
