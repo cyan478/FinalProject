9c373b71b3d6f1a44e8acaec00c6528e8d87110f
cyan478 <cyan2@stuy.edu>
2016-01-21 20:36:58 +0000

    started opponent

--- Opponent.java
+++ Opponent.java
@@ -1,5 +1,9 @@
 public class Opponent extends Character{
 
-
+    public Opponent() {
+        super();
+        
+    }
+    
 
 } //end
--- Player.java
+++ Player.java
@@ -1,7 +1,12 @@
 public class Player extends Character{
- 
+    
+    private ArrayList<String> bag;
+    
     public Player(String name){
 	super(name);
+	
+	bag = new ArrayList<string>();
+	bag.add("Oran berry");
 	
     }
     
@@ -10,16 +15,20 @@ public class Player extends Character{
     //gainEXP(x) -> if _exp+=exp > _exp limit, levelUp()
     public void Update(int exp){
 	gainEXP(exp);
-	if (this._currentexp > this._exp) 
-	
-
+	if (this._currentexp >= this._exp){
+	    levelUp();
+	    _currentexp = 0;
+	}
+	}
 
     //Gaining EXP (Experience Points used to level up)
+    //===================================================
     public void gainEXP(int exp){
 	this._currentexp += exp;
     }
 
     //Leveling up 
+    //===========
     public void levelUp(){
     	int h,a,d;
     	h = (int)(Math.random() * 10);
@@ -31,7 +40,8 @@ public class Player extends Character{
     	_def += d;
     	_level += 1;
     }
-
+    
+    
 
 
 
