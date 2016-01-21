938eb9ad9d5583896a60a3676809f9e6e286d697
nancycao <nancycao717@gmail.com>
2016-01-21 03:41:37 -0500

    incomplete

--- Map_Level1.java
+++ Map_Level1.java
@@ -1,18 +1,23 @@
 public class Map_Level1 extends Map {
   
-  public Map_Level1() {
+    public Map_Level1() {
     
-    super();
+	super();
+	setMap(new Tile [2][2]);
+	String [][] a = {
+	    {"O","O"},
+	    {"O","^"}
+	};
+	setStringMap(a);
     
-    for (int i = 0; i < _stringMap.length; i++) {
-      for (int j = 0; j < _stringMap[i].length; j++) {
-        this._map[i][j] = new Tile(i, j, _stringMap[i][j]);
-      }
+	for (int i = 0; i < 2; i++) {
+	    for (int j = 0; j < 2; j++) 
+		setTile(i, j);
+	}
     }
-  }
   
-  public static void main(String[] args) {
-    Map_Level1 a = new Map_Level1();
-    System.out.println(a);
-  }
+    public static void main(String[] args) {
+	Map_Level1 a = new Map_Level1();
+	System.out.println(a);
+    }
 }
